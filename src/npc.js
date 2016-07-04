bf0e8633061df05a1658b2c25d0ded2dee748042
KayandTheBlack <victor.gimenez.abalos@gmail.com>
2016-07-04 13:06:24 +0200

    Coding intesifies

--- src/npc.js
+++ src/npc.js
@@ -1,4 +1,6 @@
 const C = require('./constants.js')
+// const utils = require('./utils.js')
+
 class BasicAI {
   constructor (name, alignment, spawnpos, baselife, baseattack, speed, difficulty, sprite) {
     this.name = name
@@ -12,16 +14,16 @@ class BasicAI {
     this.critModifier = 2.5
     this.alignment = alignment
   }
-  objectiveTile(board) {
+  objectiveTile (board) {
     let posTiles = []
     let r = 0
-    var Tile = { i:0, j:0 }
+    var Tile = { i: 0, j: 0 }
     while (r < 4) {
       if (r === 0) {
-        Tile = { i:1, j:0 }
-      } else if (r === 1) Tile = { i:-1, j:0 }
-      else if (r === 2) Tile = { i:0, j:1 }
-      else  Tile = { i:0, j:-1 }
+        Tile = { i: 1, j: 0 }
+      } else if (r === 1) Tile = { i: -1, j: 0 }
+      else if (r === 2) Tile = { i: 0, j: 1 }
+      else Tile = { i: 0, j: -1 }
       Tile.i += this.position.i
       Tile.j += this.position.j
       if (Tile.i > 0 && Tile.i < board.length && Tile.j > 0 && Tile.j < board[0].length) {
@@ -29,25 +31,52 @@ class BasicAI {
       }
       r++
     }
-    if (posTiles.length === 0) console.log('FATAL ERROR, got no place to advance for ' + this.name + ' in ' this.position)
+    if (posTiles.length === 0) console.log('FATAL ERROR, got no place to advance for ' + this.name + ' in ' + this.position)
     if (posTiles.length === 1) return posTiles[0]
     else {
-      return closestToCenterEmptywhatever
+      let chosen = false
+      let i = 0
+      while (!chosen && i < posTiles.length) {
+        if (board[posTiles[i].i][posTiles[i].j] !== C.EMPTY_CELL) chosen = true
+        else i++
+      }
+      if (chosen) return posTiles[i]
+      else {
+        i = 1
+        var centermostpos = posTiles[0]
+        while (i < posTiles.length) {
+          if (Math.abs(posTiles[i].i - board.length / 2) > Math.abs(centermostpos.i - board.length / 2) || Math.abs(posTiles[i].j - board[0].length / 2) > Math.abs(centermostpos.j - board[0].length / 2)) {
+            centermostpos = posTiles[i]
+          }
+        }
+        return centermostpos
+      }
     }
   }
-  advance(board) {
-    var newPos = objectiveTile(board)
+  advance (board, pos) {
     this.lastPosition = this.position
-    this.position = Tile
-    if (board[Tile.i][Tile.j] === C.EMPTY_CELL) board[Tile.i][Tile.j] = [this]
-    else board[Tile.i][Tile.j] += this
+    this.position = pos
+    if (board[pos.i][pos.j] === C.EMPTY_CELL) board[pos.i][pos.j] = [this]
+    else board[pos.i][pos.j] += this
   }
-  attack() {
-    if (Math.Random() < critChance) return Math.round(this.attack * this.critModifier + Math.Random() * 3 - 1)
+  attack () {
+    if (Math.Random() < this.critChance) return Math.round(this.attack * this.critModifier + Math.Random() * 3 - 1)
     else return Math.round(this.attack + Math.Random() * 3 - 1)
   }
-  tickTurn(board, npcPos) {
+  tickTurn (board) {
     var objectiveTile = objectiveTile(board)
-
+    if (board[objectiveTile.i][objectiveTile.j] === C.EMPTY_CELL) {
+      this.advance(board, objectiveTile)
+    } else {
+      if (board[objectiveTile.i][objectiveTile.j][0].alignment === this.alignment) this.advance(board, objectiveTile)
+      else {
+        board[objectiveTile.i][objectiveTile.j][0].harm(this.attack)
+      }
+    }
+  }
+  harm (attack) {
+    this.life -= attack
+    if (this.life <= 0) console.log()
   }
 }
+exports.BasicAI = BasicAI
